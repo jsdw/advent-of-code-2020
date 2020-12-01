7341315641f7701594b6c6aa550c5d88fb9b962d
James Wilson <james.wilson@prowler.io>
2020-12-01 09:33:40 +0000

    day1 star2

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -10,15 +10,29 @@ struct Opts {
 fn main() -> Result<(),anyhow::Error> {
     let opts = Opts::from_args();
 
+    let nums: Vec<i64> = opts.file.lines().filter_map(|s| s.parse().ok()).collect();
+
     // Find two numbers that sum to 2020:
-    let mut ns = HashSet::new();
-    for line in opts.file.lines() {
-        let n: i64 = line.parse()?;
+    let mut seen = HashSet::new();
+    for &n in nums.iter() {
         let other = 2020 - n;
-        if ns.contains(&other) {
-            println!("Star 1: {}", n * other)
+        if seen.contains(&other) {
+            println!("Star 1: {}", n * other);
+            break
         } else {
-            ns.insert(n);
+            seen.insert(n);
+        }
+    }
+
+    // No cleverness here
+    for i in 0..nums.len() {
+        for j in i..nums.len() {
+            for k in j..nums.len() {
+                if nums[i]+nums[j]+nums[k] == 2020 {
+                    println!("Star 2: {}", nums[i]*nums[j]*nums[k]);
+                    break
+                }
+            }
         }
     }
 
